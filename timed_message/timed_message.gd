a69ab9cadfbc30990c2cae2e100874d1dddb9afa
SeukWon Kang <kasworld@gmail.com>
2023-11-11 13:36:54 +0900

    update timed message

--- timed_message/timed_message.gd
+++ timed_message/timed_message.gd
@@ -1,5 +1,10 @@
 extends PanelContainer
 
+func init(rt :Rect2, ver :String)->void:
+	position = rt.position
+	size = rt.size
+	$VBoxContainer/VersionLabel.text = ver
+
 func show_message(msg :String, sec :float = 3)->void:
 	$VBoxContainer/Label.text = msg
 	visible = true
